cf5bcbfa51b1eac2032034aa0a1b252b077a1806
Mohamed Kari <contact@mkari.de>
2020-05-07 01:18:27 +0200

    use ugly redundant vars for sm

--- models/FlowNet2_Models/channelnorm_package/make.sh
+++ models/FlowNet2_Models/channelnorm_package/make.sh
@@ -6,7 +6,13 @@ echo "Compiling channelnorm kernels by nvcc..."
 rm -f ChannelNorm_kernel.o
 rm -r ../_ext
 
-nvcc -c -o ChannelNorm_kernel.o ChannelNorm_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52 -I ${TORCH}/lib/include/TH -I ${TORCH}/lib/include/THC
+# Look up your Compute Compatibility Version for GPU under https://en.wikipedia.org/wiki/CUDA#GPUs_supported
+# Removing the dot between major and minor version yields the SM (System Management) value for the arch flag
+
+tesla_k80=37
+tesla_p4=61
+
+nvcc -c -o ChannelNorm_kernel.o ChannelNorm_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_${tesla_p4} -I ${TORCH}/lib/include/TH -I ${TORCH}/lib/include/THC
 
 cd ../
 python3 build.py
--- models/FlowNet2_Models/correlation_package/make.sh
+++ models/FlowNet2_Models/correlation_package/make.sh
@@ -8,7 +8,13 @@ echo "Compiling correlation kernels by nvcc..."
 rm -f correlation_cuda_kernel.o
 rm -r ../_ext
 
-nvcc -c -o correlation_cuda_kernel.o correlation_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
+# Look up your Compute Compatibility Version for GPU under https://en.wikipedia.org/wiki/CUDA#GPUs_supported
+# Removing the dot between major and minor version yields the SM (System Management) value for the arch flag
+
+tesla_k80=37
+tesla_p4=61
+
+nvcc -c -o correlation_cuda_kernel.o correlation_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_${tesla_k80}
 
 cd ../
 python3 build.py
