b88d6a3235f7414c7051b9d751ca36a0a6ca5458
Mohamed Kari <contact@mkari.de>
2020-05-27 13:09:51 +0200

    target Tesla T4 (EC2 g4dn.large type) arch

--- models/correlation_package/make.sh
+++ models/correlation_package/make.sh
@@ -8,7 +8,7 @@ echo "Compiling correlation kernels by nvcc..."
 rm -f correlation_cuda_kernel.o
 rm -rf ../_ext
 
-nvcc -c -o correlation_cuda_kernel.o correlation_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
+nvcc -c -o correlation_cuda_kernel.o correlation_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_75
 
 cd ../
 python build.py
