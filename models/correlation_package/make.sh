767d1fc525e42914ece811569a38c885dd1fd80b
Mohamed Kari <contact@mkari.de>
2020-05-27 13:05:41 +0200

    avoid useless error messges

--- models/correlation_package/make.sh
+++ models/correlation_package/make.sh
@@ -5,8 +5,8 @@ cd src
 
 echo "Compiling correlation kernels by nvcc..."
 
-rm correlation_cuda_kernel.o
-rm -r ../_ext
+rm -f correlation_cuda_kernel.o
+rm -rf ../_ext
 
 nvcc -c -o correlation_cuda_kernel.o correlation_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
 
